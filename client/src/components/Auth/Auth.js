8a21d62d486e7c195fd45285db1bc1f128af3a06
antonio-lopez <pezlop202@gmail.com>
2022-01-18 19:51:21 -0800

    ternary operator with login and register user

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -4,6 +4,7 @@ import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
 const initialState = {
+  name: '',
   email: '',
   password: '',
 };
@@ -12,11 +13,16 @@ const Auth = () => {
   const dispatch = useDispatch();
   const history = useHistory();
   const [formData, setFormData] = useState(initialState);
+  const [isSignUp, setIsSignUp] = useState(true);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log('handleSubmit', formData);
-    dispatch(login(formData, history));
+    if (isSignUp) {
+      dispatch(register(formData, history));
+    } else {
+      dispatch(login(formData, history));
+    }
   };
 
   const handleChange = (e) => {
@@ -25,10 +31,20 @@ const Auth = () => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
+  const switchMode = () => {
+    setIsSignUp((prevIsSignUp) => !prevIsSignUp);
+  };
+
   return (
     <div>
       <h1>Auth component</h1>
       <form onSubmit={handleSubmit}>
+        {isSignUp && (
+          <div>
+            <label name='name'>Username</label>
+            <input type='text' name='name' onChange={handleChange} required />
+          </div>
+        )}
         <div>
           <label name='email'>Email</label>
           <input type='email' name='email' onChange={handleChange} required />
@@ -42,7 +58,12 @@ const Auth = () => {
             required
           />
         </div>
-        <button type='submit'>Sign In</button>
+        <button type='submit'>{isSignUp ? 'Sign Up' : 'Sign In'}</button>
+        <button onClick={switchMode}>
+          {isSignUp
+            ? 'Already have an account? Sign In'
+            : "Don't have an account? Sign Up"}
+        </button>
       </form>
     </div>
   );
